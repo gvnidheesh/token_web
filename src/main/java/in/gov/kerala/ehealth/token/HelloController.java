f569d4a1643a00a7b2bf016f7c57a2ce660838db
Nidheesh G <nidheesh.ehk@kerala.gov.in>
2025-07-05 10:47:56 +0530

    Update Jenkinsfile

--- src/main/java/in/gov/kerala/ehealth/token/HelloController.java
+++ src/main/java/in/gov/kerala/ehealth/token/HelloController.java
@@ -1,13 +1,27 @@
 package in.gov.kerala.ehealth.token;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import in.gov.kerala.ehealth.token.config.AppProperties;
 
 @Controller
 public class HelloController {
+	
+	@Autowired
+    private AppProperties appProperties;
 
-    @GetMapping("/hello")
-    public String hello() {
+	    public HelloController(AppProperties appProperties) {
+	        this.appProperties = appProperties;
+	    } 
+	
+	 
+	 @GetMapping("/hello")  
+		 public String hello(Model model) {
+		  model.addAttribute("appName", appProperties.getName());
+	        model.addAttribute("appVersion", appProperties.getVersion());
         return "hello"; // maps to hello.html in templates folder
     }
+    
 }
---
+++ src/main/java/in/gov/kerala/ehealth/token/config/AppProperties.java
@@ -0,0 +1,30 @@
+package in.gov.kerala.ehealth.token.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+    private String name;
+    private String version;
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
